1f7d447fd3bf8d1845bf2957b80f65ed5ca2595b
Albert Bakhitov <bakhitov-ai@zvuk.com>
2025-04-02 01:12:42 +0400

    Поправил замечания

--- LessonsSwiftUITests/LessonsSwiftUITests.swift
+++ LessonsSwiftUITests/LessonsSwiftUITests.swift
@@ -9,24 +9,9 @@ import XCTest
 
 final class LessonsSwiftUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
-
-    override func tearDownWithError() throws {
-    }
-
     func testExample() throws {
         let app = XCUIApplication()
         app.launch()
         XCTAssertEqual(app.state, .runningBackground, "Приложение не запустилось")
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
