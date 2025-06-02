88bd6184cec24f7cf1edf330891a58f4939700e5
Albert Bakhitov <bakhitov-ai@zvuk.com>
2025-06-02 23:09:18 +0400

    Добавил тест проверки переключения свитчера и отображения элементов экрана при переключении

---
+++ LessonsSwiftUITests/Tests/TapToSwitcherTest.swift
@@ -0,0 +1,22 @@
+import XCTest
+
+final class TapToSwitcherTest: BaseTest {
+    func testLessonSwift() {
+        startTests()
+        step("Переключение свитчера и отображение элементов") {
+            step("Элементы отображаются, свитчер включен") {
+                XCTAssertEqual(PersonViewControllerScreen.personSwitcher.value as? String, "1", "Свитчер включен")
+                XCTAssertFalse(PersonViewControllerScreen.personName.exists == false)
+                XCTAssertFalse(PersonViewControllerScreen.personLastName.exists == false)
+            }
+            step("Свитчер выключен") {
+                PersonViewControllerScreen.personSwitcher.tap()
+            }
+            step("Свитчер переключен, элементы не отображаются") {
+                XCTAssertEqual(PersonViewControllerScreen.personSwitcher.value as? String, "0", "Свитчер выключен")
+                XCTAssertFalse(PersonViewControllerScreen.personName.isHittable)
+                XCTAssertFalse(PersonViewControllerScreen.personLastName.isHittable)
+            }
+        }
+    }
+}
